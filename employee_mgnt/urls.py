a246501bb485c600fa892607dff7beac20c0f9fe
Sreerag P <sreeragp10@gmail.com>
2025-02-10 21:38:10 +0530

    create home app & linked other pages , changed css'

--- employee_mgnt/urls.py
+++ employee_mgnt/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('api/auth/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
 
