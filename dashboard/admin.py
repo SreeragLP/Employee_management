aa25a7e26e0f8b781b45ea51d03e5e553c3295af
Sreerag P <sreeragp10@gmail.com>
2025-02-11 16:59:52 +0530

    fixed css of dynamic form,and register admin,

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DynamicForm, SubmittedForm
 
 # Register your models here.
+
+admin.site.register(DynamicForm)
+admin.site.register(SubmittedForm)
